11ae16f03acca6f4ce3eceafc3052815ebda1def
Sergei <schemberev@gmail.com>
2024-04-04 20:16:28 +0300

    Update depositPercentCounter.java
    Доделал нормальное представление в командной строке + вывод в файл (когда то допилю до табличного представления)

--- untitled/src/depositPercentCounter.java
+++ untitled/src/depositPercentCounter.java
@@ -4,20 +4,17 @@
     SChemberev 31.03.24
  */
 
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 
 public class depositPercentCounter
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         Scanner inp = new Scanner(System.in);
         System.out.print("Введите количество лет для подсчета: ");
-        double years = inp.nextDouble();
+        double years = inp.nextDouble() + 1;
         System.out.print("Введите первоначальную сумму вклада: ");
         double deposit = inp.nextDouble();
-        //String countChars = String.valueOf(deposit);
-        //System.out.println(countChars.length());
         double[][] table = new double[(int) years][20];
         for(int i = 0; i < years; i++) //цикл для заполнения массива
         {
@@ -33,33 +30,52 @@ public class depositPercentCounter
                 }
             }
         }
-        String countChars = String.valueOf(table[0][0]);
-        System.out.println(countChars.length() + 3);
-        System.out.print("   | "); //формирование табличного представления (начало)
-        for (int i = 1; i <= 20; i++) //цикл для вывода первой строки таблицы с процентами
-        {
-            System.out.print(i + "%");
-            for(int j = 0; j < countChars.length() + 3; j++)
-            {
-                 if(i <= 10)
-                 {
-                     System.out.print(" ");
-                 }
-                 else
-                 {
-                     System.out.print(" ");
-                 }
-            }
-        }
         System.out.println();
         for(int i = 0; i < years; i++) //вывод двумерного массива
         {
-            System.out.print(i + 1 + "  |");
-            for (int j = 0; j < 20; j++)
+            if(i < 10)
             {
-                System.out.print(" " + String.format("%.4f", table[i][j]) + " ");
+                System.out.print(i + "   |");
+            }
+            else
+            {
+                System.out.print(i + "  |");
+            }
+            for (int j = 0; j < 20; j++) {
+                if (i == 0) {
+                    System.out.format( "%10s", j + 1 + "%");
+                } else {
+                    System.out.format("%10s", String.format("%.2f", table[i][j]));
+                }
             }
             System.out.println();
+        }
+        File outFile = new File("D:/depositCounter.txt");
+        outFile.createNewFile();
+        // FileOutputStream newOutput = new FileOutputStream(outFile);
+        saveArrayToFile(table, outFile);
+
+    }
+
+    private static void saveArrayToFile(double[][] arr, File path) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+            bw.write(String.valueOf(arr.length));
+            bw.newLine();
+            bw.write(String.valueOf(arr[0].length));
+            bw.newLine();
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    bw.write(String.valueOf(arr[i][j]));
+                    bw.newLine();
+                }
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            // тут совсем не обязательно писать System.out.println, оно и так выведет ошибку в консоль
+            e.printStackTrace();
         }
     }
 }
