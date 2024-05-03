0baa83fb2d343bf58710c30c57b201e700573c2c
Sergei <schemberev@gmail.com>
2024-05-03 19:13:32 +0300

    Vectors
    Доделал задание по векторам

--- Vector/src/Vector.java
+++ Vector/src/Vector.java
@@ -37,18 +37,31 @@ public class Vector {
         return scalar;
     }
 
-    public double vectorLen(Vector v1){ // вычисление длины вектора
+    public double vectorLen(){ // вычисление длины вектора
         double vectorLen;
         vectorLen = Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2) + Math.pow(getZ(), 2));
         return vectorLen;
     }
 
-    public Vector[] vectorMul(Vector v1, Vector v2){
+    public String vectorMul(Vector v1, Vector v2){ // векторное произведение
         double x = v1.getY() * v2.getZ() - v1.getZ() * v2.getY();
         double y = v1.getZ() * v2.getX() - v1.getX() * v2.getZ();
         double z = v1.getX() * v2.getY() - v1.getY() * v2.getX();
         Vector newVector = new Vector(x, y, z);
-        return newVector[];
+        return ("("+ newVector.getX() + "," + newVector.getY() + "," + newVector.getZ() + ")");
+        //return new Vector(x, y, z);
     }
 
+    public static Vector[] newVectorArr (int n){ // создание массива размерностью n новых векторов
+        Vector[] nVector = new Vector[n];
+        for (int i = 0; i < n; i++){
+            nVector[i] = new Vector(Math.random(), Math.random(), Math.random());
+        }
+        return nVector;
+    }
+
+    public double cosVec (Vector v1, Vector v2){
+       double cosVec = scalarProduct(v1, v2) / v1.vectorLen() * v2.vectorLen();
+       return  cosVec;
+    }
 }
