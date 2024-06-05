0a807c89dcfe8394d19165f59177e0f810da9416
Sergei <schemberev@gmail.com>
2024-06-05 20:06:49 +0300

    третье задание
    Функция Аккермана рекурсивно

--- recursion/src/Recursion.java
+++ recursion/src/Recursion.java
@@ -22,4 +22,16 @@ public class Recursion {
             return aToB(A + 1, B) + " " + B;
         }
     }
+
+    public int Akkerman(int m, int n){ // Функция Аккермана
+        if (m == 0){
+            return (n + 1);
+        }
+        else if (m > 0 && n == 0){
+            return (Akkerman(m - 1, 1));
+        }
+        else{
+            return (Akkerman(m - 1, Akkerman(m, n - 1)));
+        }
+    }
 }
--- recursion/src/main.java
+++ recursion/src/main.java
@@ -3,5 +3,6 @@ public class main {
         Recursion rec = new Recursion();
         System.out.println(rec.nString(10));
         System.out.println(rec.aToB(19, 10));
+        System.out.println(rec.Akkerman(1,4));
     }
 }
